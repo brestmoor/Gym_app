67e98a2f4a944f785c88d23b30daf0b2c5caeb45
brestmoor <filson8@gmail.com>
2016-11-09 00:18:58 +0100

    days of the week load properly

--- static/Gym_app/js/page_load.js
+++ static/Gym_app/js/page_load.js
@@ -24,9 +24,15 @@ var loadDaysOfTheWeek = function () {
         url: "http://localhost:8000/week/days",
         type: "GET",
         dataType: "json"
-    }).done(function (json) {
-        alert(json);
+    }).done(function (days) {
+        fillScheduleDayNames(JSON.parse(days))
     }).fail(function (xhr, status, errorThrown) {
         alert("Error" + "**" + xhr + "**" + status + "**" + errorThrown);
     })
 };
+
+var fillScheduleDayNames = function (days) {
+    $(".day").each(function(index, object){
+        $(this).text(days[index])
+    })
+}
